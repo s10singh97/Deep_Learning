0d82ae2315baab0f323346a874cfd64d9d9f7716
s10singh97 <s10singh97@yahoo.in>
2018-06-23 15:54:14 +0530

    Predicting Google Stock Prices

--- Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 3 - Recurrent Neural Networks (RNN)/Section 12 - Building a RNN/my_rnn.py
+++ Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 3 - Recurrent Neural Networks (RNN)/Section 12 - Building a RNN/my_rnn.py
@@ -53,3 +53,25 @@ regressor.add(Dense(units = 1))
 regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
 # Fitting the RNN to the training set
 regressor.fit(X_train, y_train, batch_size = 32, epochs = 100)
+
+# Getting Real Stock Price of 2017
+dataset_test = pd.read_csv("Google_Stock_Price_Test.csv")
+real_stock_price = dataset_test.iloc[:, 1:2].values
+
+# Getting Predicted Stock Price of 2017
+dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis = 0)
+inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
+inputs = inputs.reshape(-1, 1)
+inputs = sc.transform(inputs)
+X_test = []
+for i in range(60, 80):
+    X_test.append(inputs[i-60:i, 0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+predicted_stock_price = regressor.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
+
+
+
+
