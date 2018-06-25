6ae3eac682a4d70d602489c6f4204e9dc541f5b8
s10singh97 <s10singh97@yahoo.in>
2018-06-25 12:24:33 +0530

    Using MiniSom and training the data

--- Deep_Learning_A_Z/Volume 2 - Unsupervised Deep Learning/Part 4 - Self Organizing Maps (SOM)/Section 16 - Building a SOM/Self_Organising_Maps/my_som.py
+++ Deep_Learning_A_Z/Volume 2 - Unsupervised Deep Learning/Part 4 - Self Organizing Maps (SOM)/Section 16 - Building a SOM/Self_Organising_Maps/my_som.py
@@ -17,3 +17,8 @@ y = dataset.iloc[:, -1].values
 from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler(feature_range = (0,1))
 X = sc.fit_transform(X)
+
+from minisom import MiniSom
+som = MiniSom(x = 10, y = 10, input_len = 15, sigma = 1.0, learning_rate = 0.5)
+som.random_weights_init(X)
+som.train_random(data = X, num_iteration = 100)
