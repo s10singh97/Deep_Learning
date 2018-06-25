f38edae83067a1b186bd6074dbeac99f54a1f936
s10singh97 <s10singh97@yahoo.in>
2018-06-25 11:51:30 +0530

    dataset for som

---
+++ Deep_Learning_A_Z/Volume 2 - Unsupervised Deep Learning/Part 4 - Self Organizing Maps (SOM)/Section 16 - Building a SOM/Self_Organising_Maps/my_som.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 25 11:47:07 2018
+
+@author: Shashwat
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Credit_Card_Applications.csv')
