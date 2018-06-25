85223446305997c4c0cea5e316ecd5b8d4361f89
s10singh97 <s10singh97@yahoo.in>
2018-06-25 17:35:53 +0530

    Constructing ANN over SOM

--- Deep_Learning_A_Z/Volume 2 - Unsupervised Deep Learning/Part 4 - Self Organizing Maps (SOM)/Section 16 - Building a SOM/Mega_Case_Study/my_mega_case_study.py
+++ Deep_Learning_A_Z/Volume 2 - Unsupervised Deep Learning/Part 4 - Self Organizing Maps (SOM)/Section 16 - Building a SOM/Mega_Case_Study/my_mega_case_study.py
@@ -5,3 +5,75 @@ Created on Mon Jun 25 17:03:40 2018
 @author: Shashwat
 """
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Credit_Card_Applications.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Feature Scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0,1))
+X = sc.fit_transform(X)
+
+# Training the SOM
+from minisom import MiniSom
+som = MiniSom(x = 10, y = 10, input_len = 15, sigma = 1.0, learning_rate = 0.5)
+som.random_weights_init(X)
+som.train_random(data = X, num_iteration = 100)
+
+# Visualising the results
+from pylab import bone, pcolor, colorbar, show, plot
+bone()
+pcolor(som.distance_map().T)
+colorbar()
+markers = ['o', 's']
+colors = ['r', 'g']
+for i, x in enumerate(X):
+    w = som.winner(x)
+    plot(w[0] + 0.5, w[1] + 0.5, markers[y[i]], markeredgecolor = colors[y[i]], markerfacecolor = 'None', markersize = 10, markeredgewidth = 2)
+show()
+
+# Catch the Culprits
+mappings = som.win_map(X)
+frauds = np.concatenate((mappings[(3,1)], mappings[(5,5)]), axis = 0)
+frauds = sc.inverse_transform(frauds)
+
+# Unsupervised to Supervised Deep Learning
+customers = dataset.iloc[:, 1:].values
+# Dependent Variable
+is_fraud = np.zeros(len(dataset))
+for i in range(0, len(dataset)):
+    if dataset['CustomerID'][i] in frauds:
+        is_fraud[i] = 1
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+customers = sc.fit_transform(customers)
+
+# Part 2 - Now let's make the ANN!
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initialising the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(units = 2, kernel_initializer = 'uniform', activation = 'relu', input_dim = 15))
+
+# Adding the output layer
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Fitting the ANN to the Training set
+classifier.fit(customers, is_fraud, batch_size = 1, epochs = 2)
+
+# Predict Probabilities of Fraud
+y_pred = classifier.predict(customers)
