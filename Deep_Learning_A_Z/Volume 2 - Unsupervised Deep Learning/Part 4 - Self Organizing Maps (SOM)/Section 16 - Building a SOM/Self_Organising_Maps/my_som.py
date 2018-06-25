69a26f0f39910c3d62b21612610aeb2f549343bf
s10singh97 <s10singh97@yahoo.in>
2018-06-25 12:03:25 +0530

    Feature Scaling

--- Deep_Learning_A_Z/Volume 2 - Unsupervised Deep Learning/Part 4 - Self Organizing Maps (SOM)/Section 16 - Building a SOM/Self_Organising_Maps/my_som.py
+++ Deep_Learning_A_Z/Volume 2 - Unsupervised Deep Learning/Part 4 - Self Organizing Maps (SOM)/Section 16 - Building a SOM/Self_Organising_Maps/my_som.py
@@ -10,3 +10,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 dataset = pd.read_csv('Credit_Card_Applications.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Feature Scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0,1))
+X = sc.fit_transform(X)
