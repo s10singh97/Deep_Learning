747f023e1196196f227717148ff97cde942180cb
s10singh97 <s10singh97@yahoo.in>
2018-06-28 16:15:18 +0530

    Preparing test set and trining set

--- Deep_Learning_A_Z/Volume 2 - Unsupervised Deep Learning/Part 5 - Boltzmann Machines (BM)/Section 19 - Building a Boltzmann Machine/my_rbm.py
+++ Deep_Learning_A_Z/Volume 2 - Unsupervised Deep Learning/Part 5 - Boltzmann Machines (BM)/Section 19 - Building a Boltzmann Machine/my_rbm.py
@@ -17,3 +17,10 @@ from torch.autograd import Variable
 movies = pd.read_csv('ml-1m/movies.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
 users = pd.read_csv('ml-1m/users.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
 ratings = pd.read_csv('ml-1m/ratings.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
+
+# Preparing the training set and test test
+training_set = pd.read_csv('ml-100k/u1.base', delimiter = '\t')
+training_set = np.array(training_set, dtype = 'int')
+
+test_set = pd.read_csv('ml-100k/u1.test', delimiter = '\t')
+test_set = np.array(test_set, dtype = 'int')
