a41a0bd5b77747f1f2fca05fcdee43a49661dee1
s10singh97 <s10singh97@yahoo.in>
2018-06-28 16:25:39 +0530

    Getting the numbers of Users and Ratings for k-fold cross validation

--- Deep_Learning_A_Z/Volume 2 - Unsupervised Deep Learning/Part 5 - Boltzmann Machines (BM)/Section 19 - Building a Boltzmann Machine/my_rbm.py
+++ Deep_Learning_A_Z/Volume 2 - Unsupervised Deep Learning/Part 5 - Boltzmann Machines (BM)/Section 19 - Building a Boltzmann Machine/my_rbm.py
@@ -21,6 +21,9 @@ ratings = pd.read_csv('ml-1m/ratings.dat', sep = '::', header = None, engine = '
 # Preparing the training set and test test
 training_set = pd.read_csv('ml-100k/u1.base', delimiter = '\t')
 training_set = np.array(training_set, dtype = 'int')
-
 test_set = pd.read_csv('ml-100k/u1.test', delimiter = '\t')
 test_set = np.array(test_set, dtype = 'int')
+
+# Getting the numbers of Users and Ratings
+nb_users = int(max(max(training_set[:, 0]), max(test_set[:, 0])))
+nb_movies =  int(max(max(training_set[:, 1]), max(test_set[:, 1])))
