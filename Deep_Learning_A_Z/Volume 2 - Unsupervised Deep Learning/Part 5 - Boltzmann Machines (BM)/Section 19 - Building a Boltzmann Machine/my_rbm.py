adda4a7d6b0cc63b77ab4c570f2077e2eb71b5d6
s10singh97 <s10singh97@yahoo.in>
2018-06-29 13:18:52 +0530

    Testing the RBM

--- Deep_Learning_A_Z/Volume 2 - Unsupervised Deep Learning/Part 5 - Boltzmann Machines (BM)/Section 19 - Building a Boltzmann Machine/my_rbm.py
+++ Deep_Learning_A_Z/Volume 2 - Unsupervised Deep Learning/Part 5 - Boltzmann Machines (BM)/Section 19 - Building a Boltzmann Machine/my_rbm.py
@@ -99,13 +99,15 @@ for epoch in range(1, nb_epoch + 1):
         s += 1.
     print('epoch: '+str(epoch)+' loss: '+str(train_loss/s))
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+# Tesing the RBM
+test_loss = 0
+s = 0.
+for id_user in range(nb_users):
+    v = training_set[id_user:id_user + 1]
+    vt = test_set[id_user:id_user + 1]
+    if len(vt[vt >= 0]) > 0:
+        _,h = rbm.sample_h(v)
+        _,v = rbm.sample_v(h)
+        test_loss += torch.mean(torch.abs(vt[vt >= 0] - v[vt >= 0]))
+        s += 1.
+print('test loss: '+str(test_loss/s))
