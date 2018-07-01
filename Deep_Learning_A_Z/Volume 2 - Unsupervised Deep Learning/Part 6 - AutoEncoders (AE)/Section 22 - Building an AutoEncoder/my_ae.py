93e1ca447ca3a2df2174caeade34f069373ddf30
s10singh97 <s10singh97@yahoo.in>
2018-07-01 18:29:40 +0530

    Training SAE

--- Deep_Learning_A_Z/Volume 2 - Unsupervised Deep Learning/Part 6 - AutoEncoders (AE)/Section 22 - Building an AutoEncoder/my_ae.py
+++ Deep_Learning_A_Z/Volume 2 - Unsupervised Deep Learning/Part 6 - AutoEncoders (AE)/Section 22 - Building an AutoEncoder/my_ae.py
@@ -62,20 +62,24 @@ class SAE(nn.Module):
         return x
 sae = SAE()
 criterion = nn.MSELoss()
-optimizer = optim.RMSProp(sae.parameters(), lr = 0.01, weight_decay = 0.5)
+optimizer = optim.RMSprop(sae.parameters(), lr = 0.01, weight_decay = 0.5)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Training the SAE
+nb_epoch = 200
+for epoch in range(1, nb_epoch + 1):
+    train_loss = 0
+    s = 0.
+    for id_user in range(nb_users):
+        input = Variable(training_set[id_user]).unsqueeze(0)
+        target = input.clone()
+        if torch.sum(target.data > 0) > 0:
+            output = sae(input)
+            target.require_grad = False
+            output[target == 0] = 0
+            loss = criterion(output, target)
+            mean_corrector = nb_movies/float(torch.sum(target.data > 0) + 1e-10)
+            loss.backward()
+            train_loss += np.sqrt(loss.data[0]*mean_corrector)
+            s += 1.
+            optimizer.step()
+    print('epoch: ' + str(epoch) + 'loss: ' + str(train_loss/s))
