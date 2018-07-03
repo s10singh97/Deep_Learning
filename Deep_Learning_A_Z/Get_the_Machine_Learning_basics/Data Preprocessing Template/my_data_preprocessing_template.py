6c3527921bc21ec734b5f905e20020a495156c35
s10singh97 <s10singh97@yahoo.in>
2018-07-03 21:32:15 +0530

    Importing Datasets

--- Deep_Learning_A_Z/Get_the_Machine_Learning_basics/Data Preprocessing Template/my_data_preprocessing_template.py
+++ Deep_Learning_A_Z/Get_the_Machine_Learning_basics/Data Preprocessing Template/my_data_preprocessing_template.py
@@ -9,3 +9,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Importing the Dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
