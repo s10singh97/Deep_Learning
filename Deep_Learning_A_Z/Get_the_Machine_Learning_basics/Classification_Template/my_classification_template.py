dfe4be6e52b8252cf468a36626eb6462beccb390
s10singh97 <s10singh97@yahoo.in>
2018-07-04 17:03:17 +0530

    Fitting LR to Training Set

--- Deep_Learning_A_Z/Get_the_Machine_Learning_basics/Classification_Template/my_classification_template.py
+++ Deep_Learning_A_Z/Get_the_Machine_Learning_basics/Classification_Template/my_classification_template.py
@@ -24,3 +24,7 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
+# Fitting Logistic Regression to the training Set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
