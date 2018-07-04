720bfee2a8d34e7f0e7e082e242c32c4effe64fd
s10singh97 <s10singh97@yahoo.in>
2018-07-04 16:59:31 +0530

    Data Precprocessing

--- Deep_Learning_A_Z/Get_the_Machine_Learning_basics/Classification_Template/my_classification_template.py
+++ Deep_Learning_A_Z/Get_the_Machine_Learning_basics/Classification_Template/my_classification_template.py
@@ -11,9 +11,16 @@ import matplotlib.pyplot as plt
 
 # Importing the Dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
 
 # Splitting the Data into Training Set and Test Set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
